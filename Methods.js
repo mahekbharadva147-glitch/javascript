cb0d295558e8c83aa64bd356116f211b4a278df7
mahek <mahekbharadva147@gmail.com>
2026-01-29 09:46:17 +0530

    [MB][Methods & templetliterals are added]

---
+++ Methods.js
@@ -0,0 +1,24 @@
+//string methods
+//length()
+//trim()
+//toUppercase()
+//toLowercase()
+//includes()
+//indexOf()
+//Slice()
+//substring()
+//replace()
+//concat()
+//split()
+
+
+let str="Hello Javascript World"
+console.log(str)
+console.log("length():",str.length)
+
+
+let trimText=str.trim();
+console.log("trim():",trimText.length)
+
+console.log("toUpperCase():",str.toUpperCase())
+console.log("toLowerCase():",str.toLowerCase())
---
+++ templetliterals.js
@@ -0,0 +1,5 @@
+let fName="Mahek";
+let lName="Bharadva";
+
+let message='hello students,i am ${fName} ${lName}';
+console.log(message);
